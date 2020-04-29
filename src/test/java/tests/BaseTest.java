f685b78dd139dab550a426059310ce81643ae071
smerdoff <eugenesmerdov17@gmail.com>
2020-04-29 11:34:43 +0300

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    private WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+    @AfterMethod(alwaysRun =true)
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
+
